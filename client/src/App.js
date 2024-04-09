92883b0efdf643d11c02b333398769265695a939
Lucas Xiao <smy240975417@gmail.com>
2024-04-09 13:24:27 +0800

    add the middleware

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import "./App.css";
+// To make API Requests:
+//  1. Use 'fetch api (already comes with JavaScript)'
+//  2. Install library 'axios'
+import axios from "axios";
+import { useEffect } from "react"; // To fetch all the lists of posts
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  useEffect(() => {
+    // Promise: run an anonymous function after receiving the data
+    //  asynchronously, wait for the request to be done
+    //  run the 'then' function inside, data received should be stored over here
+    axios.get("http://localhost:3001/posts").then((response) => {
+      console.log(response);
+
+    });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-// To parse the JSON when acted as the body request and be able to access it
-app.use(express.json());
+// MIDDLEWARE:
+// automatically whitelist API so that the connection works
+// and able to make an API request that exists in the computer from a react app
+app.use(express.json()); // To parse the JSON when acted as the body request and be able to access it
+app.use(cors());
 
-const db = require('./models')
+const db = require("./models");
 
 // Routers
-const postRouter = require('./routes/Posts')
+const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
-
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("Server running on port 3001");
   });
-})
+});
