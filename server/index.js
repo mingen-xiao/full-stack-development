232412a5c694aa3b19262b696c4fed5d73010753
Lucas Xiao <smy240975417@gmail.com>
2024-04-09 12:02:25 +0800

    simply build up the get and post functions in backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 
+// To parse the JSON when acted as the body request and be able to access it
+app.use(express.json());
+
 const db = require('./models')
+
+// Routers
+const postRouter = require('./routes/Posts')
+app.use("/posts", postRouter);
+
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
---
+++ server/routes/Posts.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const { Posts } = require("../models");
+
+// Need to use 'async...await...' when use 'sequelize'
+router.get("/", async (req, res) => {
+  // findAll(): function in 'sequelize' to get all the data in the database
+  const listOfPosts = await Posts.findAll();
+  // return the result
+  res.json(listOfPosts);
+});
+
+// Asynchronous
+router.post("/", async (req, res) => {
+  // Grab the post data from the 'body' that is sent in the request
+  // 拿到請求的數據
+  const post = req.body;
+  // Asynchronous 異步: Wait for and make sure the data to be inserted before moving forward (with the request or anything else)
+  // 把拿到的數據創建到Posts這個數據庫裏
+  await Posts.create(post); // Add this new post into the database
+  // 返回完成反饋
+  res.json(post);
+});
+
+module.exports = router;
