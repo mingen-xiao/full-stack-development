6c8580cca2347fbf85776ec7b68e758ea4a47966
Lucas Xiao <smy240975417@gmail.com>
2024-04-22 15:50:49 +0800

    correct the page navigation when the path is wrong and provide a page for user to redirect to the home page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
 import Login from "./pages/Login";
 import Registraion from "./pages/Registration";
+import PageNotFound from "./pages/PageNotFound";
 import { AuthContext } from "./helpers/AuthContext";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -82,6 +83,7 @@ function App() {
             <Route path="/post/:id" exact element={<Post />} />
             <Route path="/login" exact element={<Login />} />
             <Route path="/registration" exact element={<Registraion />} />
+            <Route path="*" exact Component={PageNotFound} />
           </Routes>
         </Router>
       </AuthContext.Provider>
