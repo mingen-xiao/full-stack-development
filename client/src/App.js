2be35069fdd0d1f5191837177748afb5818b4b8c
Lucas Xiao <smy240975417@gmail.com>
2024-04-24 15:44:51 +0800

    Redirect to the login page when logged out

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
@@ -15,6 +21,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
+  const navigate = useNavigate();
   // Keep track whether we are logged in or not
   // Get information including username & id, when validating status
   const [authState, setAuthState] = useState({
@@ -52,50 +59,50 @@ function App() {
       id: 0,
       status: false,
     });
+    // Redirect to the login page when logged out
+    navigate("/login");
   };
 
   return (
     <div className="App">
       {/* "Provider": means context provider */}
       <AuthContext.Provider value={{ authState, setAuthState }}>
-        <Router>
-          <div className="navbar">
-            <div className="links">
-              {/* Since the STATE has changed in Login.js setAuthState(true),  */}
-              {/* it knows it's logged in & no need to display the Login & Registration buttons */}
-              {!authState.status ? ( // Shows buttons separately BEFORE and AFTER "login"
-                <>
-                  <Link to="/login">Login</Link>
-                  <Link to="/registration">Registraion</Link>
-                </>
-              ) : (
-                <>
-                  <Link to="/">Home Page</Link>
-                  <Link to="/createpost">Create A Post</Link>
-                </>
-              )}
-            </div>
-            <div className="loggedInContainer">
-              <h1>{authState.username} </h1>
-              {/* Log out button appeared when logged IN */}
-              {authState.status && <button onClick={logout}> Logout</button>}
-            </div>
+        <div className="navbar">
+          <div className="links">
+            {/* Since the STATE has changed in Login.js setAuthState(true),  */}
+            {/* it knows it's logged in & no need to display the Login & Registration buttons */}
+            {!authState.status ? ( // Shows buttons separately BEFORE and AFTER "login"
+              <>
+                <Link to="/login">Login</Link>
+                <Link to="/registration">Registraion</Link>
+              </>
+            ) : (
+              <>
+                <Link to="/">Home Page</Link>
+                <Link to="/createpost">Create A Post</Link>
+              </>
+            )}
           </div>
-          <Routes>
-            {/* "/": The route for the main entry point of the website */}
-            {/* "exact": To make never renders more than one route at the same time */}
-            {/* "component": The component to render in this route */}
-            <Route path="/" exact element={<Home />} />
-            <Route path="/createpost" exact element={<CreatePost />} />
-            <Route path="/post/:id" exact element={<Post />} />
-            <Route path="/registration" exact element={<Registraion />} />
-            <Route path="/login" exact element={<Login />} />
-            <Route path="/profile/:id" exact element={<Profile />} />
-            {/* STEP 3 */}
-            <Route path="/changepassword" exact element={<ChangePassword />} />
-            <Route path="*" exact Component={PageNotFound} />
-          </Routes>
-        </Router>
+          <div className="loggedInContainer">
+            <h1>{authState.username} </h1>
+            {/* Log out button appeared when logged IN */}
+            {authState.status && <button onClick={logout}> Logout</button>}
+          </div>
+        </div>
+        <Routes>
+          {/* "/": The route for the main entry point of the website */}
+          {/* "exact": To make never renders more than one route at the same time */}
+          {/* "component": The component to render in this route */}
+          <Route path="/" exact element={<Home />} />
+          <Route path="/createpost" exact element={<CreatePost />} />
+          <Route path="/post/:id" exact element={<Post />} />
+          <Route path="/registration" exact element={<Registraion />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/profile/:id" exact element={<Profile />} />
+          {/* STEP 3 */}
+          <Route path="/changepassword" exact element={<ChangePassword />} />
+          <Route path="*" exact Component={PageNotFound} />
+        </Routes>
       </AuthContext.Provider>
     </div>
   );
